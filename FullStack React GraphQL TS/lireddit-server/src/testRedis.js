5b5f9a368ece86628726dcaf5acd800204053bec
Jason Stevenson <jstevenson72@gmail.com>
2022-02-04 07:51:12 -0600

    rollback

--- FullStack React GraphQL TS/lireddit-server/src/testRedis.js
+++
@@ -1,9 +0,0 @@
-const redis = require("redis");
-const client = redis.createClient();
-client.on("error", function (error){
-    console.log(error);
-})
-
-client.on("conect", function (error){
-    console.log("Redis Connected");
-})
